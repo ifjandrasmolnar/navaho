75f5ceba09a45cd52b2e0ee40d8c0895a74f5cdc
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-02-26 12:17:01 +0100

    [ UPPDATE ] proxy setup

--- client/nava/vite.config.js
+++ client/nava/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const serverAddress = "http://localhost:5000/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: serverAddress,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
