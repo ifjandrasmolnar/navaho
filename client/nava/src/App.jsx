2937fe6c9f5a6a42a952984ace6c29a18013c904
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-02-28 18:54:25 +0100

    [ UPDATE ] dead code cleared

--- client/nava/src/App.jsx
+++ client/nava/src/App.jsx
@@ -1,12 +1,24 @@
-import React, { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+//import { Container } from "react-bootstrap";
+import React from "react";
+import {
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
+import Navbar from "./components/NavbarComponent";
+import Main from "./components/MainTable";
+import SearchComponent from "./components/SearchComponent";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +33,27 @@ function App() {
     fetchData();
   }, []);
 
-  return <Container className="bg-dark main-container" fluid></Container>;
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    // You can perform search-related operations here
+  };
+  //return <Container className="bg-dark main-container" fluid></Container>;
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>HELLOOO </h1>
+      </div>
+      <div style={{ marginTop: "40px" }}>
+        <SearchComponent
+          placeholder={"Keresés . . ."}
+          onSearch={handleSearch}
+        />
+      </div>
+      <Main />
+    </>
+  );
 }
 
 export default App;
