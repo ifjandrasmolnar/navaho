ca88e5d4c2adcf9509bd6cebb937d4740dca4187
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-03-01 10:16:32 +0100

    [ UPDATE ] code cleaned

--- client/nava/src/App.jsx
+++ client/nava/src/App.jsx
@@ -1,24 +1,16 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { Container } from "react-bootstrap";
 import React from "react";
-import {
-  MDBCard,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardBody,
-  MDBCardImage,
-  MDBRow,
-  MDBCol,
-} from "mdb-react-ui-kit";
 import Navbar from "./components/NavbarComponent";
-import Main from "./components/MainTable";
+import DesktopMainTable from "./components/DesktopMainTable";
+import MobileMainTable from "./components/MobileMainTable";
 import SearchComponent from "./components/SearchComponent";
 
 function App() {
   const [count, setCount] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+  const [isMobileView, setIsMobileView] = React.useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,11 +25,21 @@ function App() {
     fetchData();
   }, []);
 
+  React.useEffect(() => {
+    const handleResize = () => {
+      // Check screen size and set the state accordingly
+      setIsMobileView(window.innerWidth <= 800);
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const handleSearch = (query) => {
     setSearchQuery(query);
-    // You can perform search-related operations here
   };
-  //return <Container className="bg-dark main-container" fluid></Container>;
 
   return (
     <>
@@ -51,7 +53,7 @@ function App() {
           onSearch={handleSearch}
         />
       </div>
-      <Main />
+      <div>{isMobileView ? <MobileMainTable /> : <DesktopMainTable />}</div>
     </>
   );
 }
