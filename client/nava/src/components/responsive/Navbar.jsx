5ac2153fa9ad98969f468974c525db3bddab16a4
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-03-03 22:43:51 +0100

    [ ADD ] Navbar responsive module implemented

---
+++ client/nava/src/components/responsive/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../style/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="ndiv">
+      <div className="ndiv-2">
+        <div className="ndiv-3">EGYSZERŰ KERESŐ</div>
+        <div className="ndiv-4">ÖSSZETETT KERESŐ</div>
+        <div className="ndiv-5">LELTÁRAK</div>
+        <div className="ndiv-6">GYŰJTEMÉNYEK</div>
+        <div className="ndiv-7">SZÓTÁRAK</div>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
---
+++ client/nava/src/components/responsive/Navbar2.jsx
@@ -0,0 +1,26 @@
+import "../style/Navbar2.css";
+
+function Navbar2() {
+  return (
+    <div className="nndiv">
+      <div className="nndiv-2">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/77edc970dd52a0fc443ab9aedf7e16ac1781d5890449f9a38099b08127c898d8?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+          className="nnimg"
+        />
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/3e6191638b0c2b7352368cc24012dba532ad5bcbf941d806d3bf77cd233bd94b?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+          className="nnimg-2"
+        />
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/a9d32143284578cf876399ec84e7e0e5eb4259c3c175ecead02eff88a3e7f9e3?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+          className="nnimg"
+        />
+      </div>
+    </div>
+  );
+}
+export default Navbar2;
