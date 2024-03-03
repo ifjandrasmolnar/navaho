611321f8d3e3e7b9594ac72e311c2b39503c4b7f
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-03-03 22:44:25 +0100

    [ ADD ] Table header responsive component implemented

---
+++ client/nava/src/components/shared/TableHeader.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../style/TableHeader.css';
+
+const TableHeader = () => {
+  return (
+    <div className="hdiv">
+        <div className="hdiv-2">
+            <div className="hdiv-3">
+            <div className="hdiv-4">
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/3f274eb6799a0f3184d39e54ef32e43ba7298480e3e9b9b4bd6e14a47482d0bb?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+                className="himg"
+                />
+                <div className="hdiv-5">SZŰRÉS</div>
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/9a67b8b7f49c405e9d9d539780909608cd2f4bafee3f87205f70445efeab3fc6?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+                className="himg-2"
+                />
+            </div>
+            <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/307bc43e4d816c474048911b454db7f189cc4880001015ed53e96cf94173dc29?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+                className="himg-3"
+            />
+            <div className="hdiv-6">
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/580b8aea900c477157fc17d1a0f5271e8e744ab09370ce47ebe63af7a8079cef?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+                className="himg-4"
+                />
+                <div className="hdiv-7">RENDEZÉS</div>
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/4258c08dc9cc12207a5ccca87cd2fea50e66e38181d33204100e7f04f952c6c4?apiKey=a8ed06ca193b4bbb9cb432a6e4e9e3a2&"
+                className="himg"
+                />
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TableHeader
