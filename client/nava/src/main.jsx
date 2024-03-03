6d8ce6ce3352f69c7df41d19cb8effd2f0c85cf5
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2024-03-03 22:34:28 +0100

    [ UPDATE ] MUI theme added

--- client/nava/src/main.jsx
+++ client/nava/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      md: 800,
+      xl: 1050,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
